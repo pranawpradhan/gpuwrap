7d9f712171da3c1e938b3bcf3897aacbb9b1f705
Alex Widener <awidener@studio.local>
2016-10-10 16:27:25 -0500

    gathering geometry arguments

--- gpuwrap/gpuwrap/wrapCmd.cpp
+++ gpuwrap/gpuwrap/wrapCmd.cpp
@@ -2,13 +2,22 @@
 
 #include <maya/MArgDatabase.h>
 #include <maya/MSyntax.h>
+#include <maya/MFnDagNode.h>
+#include <maya/MGlobal.h>
+#include <maya/MItSelectionList.h>
 
 const char* WrapCmd::kName = "Wrap";
+const char* WrapCmd::kNameFlagShort = "-n";
+const char* WrapCmd::kNameFlagLong = "-name";
 
-WrapCmd::WrapCmd() {}
+WrapCmd::WrapCmd() : name_("Wrap#") {}
 
 MSyntax WrapCmd::newSyntax() {
 	MSyntax syntax;
+	syntax.addFlag(kNameFlagShort, kNameFlagLong, MSyntax::kString);
+	// Use the current selection as a selection list, and pass the selection as a default argument
+	syntax.setObjectType(MSyntax::kSelectionList, 0, 255);
+	syntax.useSelectionAsDefault(true);
 	return syntax;
 }
 
@@ -22,7 +31,87 @@ bool WrapCmd::isUndoable() const {
 
 MStatus WrapCmd::doIt(const MArgList& args) {
 	MStatus status;
+
+	// Get the geometry from the command arguments
+	status = GatherCommandArguments(args);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	status = GetGeometryPaths();
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	// Create deformer
+	// Calculate binding for wrap deformer
+	// Connect the driver mesh to the wrap deformer
+	// Store all binding information on the deformer
 	return redoIt();
+}
+
+MStatus WrapCmd::GatherCommandArguments(const MArgList& args) {
+	MStatus status;
+	MArgDatabase argData(syntax(), args);
+	argData.getObjects(selectionList_);
+	if (argData.isFlagSet(kNameFlagShort)) {
+		name_ = argData.flagArgumentString(kNameFlagShort, 0, &status);
+		CHECK_MSTATUS_AND_RETURN_IT(status);
+	}
+	return MS::kSuccess;
+}
+
+MStatus WrapCmd::GetGeometryPaths() {
+	MStatus status;
+	status = selectionList_.getDagPath(selectionList_.length() - 1, pathDriver_);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	status = GetShapeNode(pathDriver_);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+
+	if (!pathDriver_.hasFn(MFn::kMesh)) {
+		MGlobal::displayError("Wrap driver must be a mesh");
+		return MS::kFailure;
+	}
+	MItSelectionList iter(selectionList_);
+	pathDriven_.clear(); // Should be empty, call it to make sure
+	for (unsigned int i = 0; i < selectionList_.length() - 1; ++i, iter.next()) {
+		MDagPath path;
+		MObject component;
+		iter.getDagPath(path, component);
+		status = GetShapeNode(path);
+		CHECK_MSTATUS_AND_RETURN_IT(status);
+		pathDriven_.append(path);
+	}
+
+	return MS::kSuccess;
+}
+
+MStatus WrapCmd::GetShapeNode(MDagPath& path, bool intermediate) {
+	MStatus status;
+	/* Check to see if it's a shape node first.*/
+	if (IsShapeNode(path)) {
+		path.pop();
+	}
+
+	if (path.hasFn(MFn::kTransform)) {
+		// If it's a transform, find out how many shape nodes are directly below it.
+		unsigned int shapeCount;
+		status = path.numberOfShapesDirectlyBelow(shapeCount);
+		CHECK_MSTATUS_AND_RETURN_IT(status);
+
+		for (unsigned int i = 0; i < shapeCount; i++) {
+			status = path.extendToShapeDirectlyBelow(i);
+			CHECK_MSTATUS_AND_RETURN_IT(status);
+			MFnDagNode fnNode(path, &status);
+			CHECK_MSTATUS_AND_RETURN_IT(status);
+			if ((!fnNode.isIntermediateObject && !intermediate) || (fnNode.isIntermediateObject && intermediate)) {
+				return MS::kSuccess;
+			}
+			// Go back to transform node and go to the next shape
+			path.pop();
+		}
+	}
+	return MS::kFailure;
+}
+
+bool IsShapeNode(MDagPath& path) {
+	return path.node().hasFn(MFn::kMesh) || 
+		   path.node().hasFn(MFn::kNurbsCurve) || 
+		   path.node().hasFn(MFn::kNurbsSurface);
 }
 
 MStatus WrapCmd::redoIt() {
--- gpuwrap/gpuwrap/wrapCmd.h
+++ gpuwrap/gpuwrap/wrapCmd.h
@@ -3,6 +3,10 @@
 
 #include <maya/MArgList.h>
 #include <maya/MPxCommand.h>
+#include <maya/MDagPath.h>
+#include <maya/MDagPathArray.h>
+#include <maya/MSelectionList.h>
+#include <maya/MDGModifier.h>
 
 /*
 	The Wrap Command is used to create new Wrap Deformers
@@ -18,6 +22,35 @@ public:
 	static void*		creator();
 	static MSyntax		newSyntax();
 	const static char*	kName;
+
+	const static char*	kNameFlagShort;
+	const static char*	kNameFlagLong;
+private:
+	/**
+		Gathers all the command arguments and sets necessary command slates
+		@param[in] args Maya MArgList
+	*/
+	MStatus GatherCommandArguments(const MArgList& args);
+	/**
+		Acquires the driver and driven dag paths from the input selection list
+	*/
+	MStatus GetGeometryPaths();
+
+	/*
+	Ensures that the given DAG path points to a non-intermediate shape node.
+	@param[in,out] path Path to a dag node that could be a transform or a shape.
+	On return, the path will be to a shape node if one exists.
+	@param[in] intermediate true to get the intermediate shape.
+	@return MStatus
+	*/
+	MStatus GetShapeNode(MDagPath& path, bool intermediate = false);
+
+	MString name_; // Name of Wrap node to create
+	MDagPath pathDriver_; // Path to the shape wrapping the other shape
+	MDagPathArray pathDriven_; // Path to the shapes being wrapped
+	MSelectionList selectionList_; // Selected command input nodes
+	MDGModifier dgMod_;
+	MObject oWrapNode_; // MObject to the wrap node in focus.
 };
 
 #endif
